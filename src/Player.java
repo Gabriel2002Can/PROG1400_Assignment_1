e58f34dddb766044f9885d90e5d7e5230dd55098
Gabriel2002Can <gabriel2002ca@gmail.com>
2024-10-14 11:49:16 -0300

    setting classes

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,9 @@
 public class Player {
+    private String playerName;
+    private int numberOfGoals;
+    private int numberOfAssists;
+
+    public Player (String playerName) {
+        this.playerName = playerName;}
 }
+
